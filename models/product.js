b572676c72ddf9751df7e372813117643cb0d83f
Sameer Ahmed Khan <68906588+sameerahmedk@users.noreply.github.com>
2023-04-01 12:30:55 +0500

    Added product ID

--- models/product.js
+++ models/product.js
@@ -1,45 +1,50 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const { randomUUID } = require('crypto');
 
 const productSchema = new mongoose.Schema({
-  productId: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  supplierId: {
-    type: String,
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  unit_price: {
-    type: Number,
-    required: true,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  brand: {
-    type: String,
-    required: true,
-  },
-  quantity: {
-    type: Number,
-    required: true,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
+	productId: {
+		type: String,
+		default: randomUUID(),
+		required: true,
+		unique: true,
+	},
+	supplierId: {
+		type: String,
+		required: true,
+	},
+	name: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+		required: true,
+	},
+	unit_price: {
+		type: Number,
+		required: true,
+	},
+	category: {
+		type: String,
+		required: true,
+	},
+	brand: {
+		type: String,
+		required: true,
+	},
+	quantity: {
+		type: Number,
+		required: true,
+	},
+	image: {
+		type: String,
+		required: true,
+	},
 });
 
-const Product = mongoose.model("Product", productSchema);
+const Product = mongoose.model(
+	'Product',
+	productSchema
+);
 
 module.exports = Product;
