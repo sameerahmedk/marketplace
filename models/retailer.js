a8ce1d64fd966f1d30d16866f8127893a483f936
Sameer Ahmed Khan <68906588+sameerahmedk@users.noreply.github.com>
2023-04-01 12:31:23 +0500

    Added ref types in model

--- models/retailer.js
+++ models/retailer.js
@@ -1,28 +1,28 @@
 const mongoose = require("mongoose");
 
 const retailerSchema = new mongoose.Schema({
-  user_id: {
-    type: String,
-    required: true,
-    ref: "User.user_id",
-  },
-  address: {
-    type: String,
-    required: true,
-  },
-  city: {
-    type: String,
-    required: true,
-  },
-  country: {
-    type: String,
-    required: true,
-  },
-  nic: {
-    type: String,
-    required: true,
-    unique: true,
-  },
+	user_id: {
+		type: mongoose.Schema.Types.String,
+		required: true,
+		ref: 'User.user_id',
+	},
+	address: {
+		type: String,
+		required: true,
+	},
+	city: {
+		type: String,
+		required: true,
+	},
+	country: {
+		type: String,
+		required: true,
+	},
+	nic: {
+		type: String,
+		required: true,
+		unique: true,
+	},
 });
 
 const Retailer = mongoose.model("Retailer", retailerSchema);
--- models/supplier.js
+++ models/supplier.js
@@ -1,32 +1,32 @@
 const mongoose = require("mongoose");
 
 const supplierSchema = new mongoose.Schema({
-  user_id: {
-    type: String,
-    required: true,
-    ref: "User.user_id",
-  },
-  address: {
-    type: String,
-    required: true,
-  },
-  city: {
-    type: String,
-    required: true,
-  },
-  country: {
-    type: String,
-    required: true,
-  },
-  nic: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  iban: {
-    type: String,
-    required: true,
-  },
+	user_id: {
+		type: mongoose.Schema.Types.String,
+		required: true,
+		ref: 'User.user_id',
+	},
+	address: {
+		type: String,
+		required: true,
+	},
+	city: {
+		type: String,
+		required: true,
+	},
+	country: {
+		type: String,
+		required: true,
+	},
+	nic: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	iban: {
+		type: String,
+		required: true,
+	},
 });
 
 const Supplier = mongoose.model("Supplier", supplierSchema);
