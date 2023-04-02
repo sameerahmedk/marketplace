7e1bad65dd379430467e71d3183ac55fdf133cd0
abdurehman12 <abdurehmanabbasi1243@gmail.com>
2023-04-03 03:23:30 +0500

    cors added

--- index.js
+++ index.js
@@ -10,16 +10,25 @@ const ProductRoute = require("./routes/ProductRoute");
 const bodyParser = require("body-parser");
 const authRouter = require("./routes/auth.routes");
 //const {verifyAccessToken} = require('./helpers/jwtHelper')
+ //var cors = require('cors')
 
 const url3 = "mongodb://127.0.0.1/Web_Project";
 
 const app = express();
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3001', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/auth", authRouter);
 app.use("/supplier", SupplierRoute);
 app.use("/retailer", RetailerRoute);
 app.use("/product", ProductRoute);
+// app.use(cors());
 
 mongoose.connect(url3, { useNewUrlParser: true, useUnifiedTopology: true });
 const con = mongoose.connection;
@@ -28,7 +37,7 @@ con.on("open", function () {
   console.log("connected...");
 });
 
-app.listen(3000, () => {
+app.listen(8080, () => {
   console.log("server started");
 });
 
--- models/role.js
+++ models/role.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const roleSchema = new mongoose.Schema({
   role_name: {
     type: String,
+    enum: ["supplier", "retailer"],
     required: true,
     unique: true,
   },
