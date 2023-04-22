2ac686090cd9171160894cb2647f8bbf218cff69
Sameer Ahmed Khan <68906588+sameerahmedk@users.noreply.github.com>
2023-04-23 02:08:02 +0500

    fixed joi

--- helpers/validationSchema.js
+++ helpers/validationSchema.js
@@ -1,21 +1,29 @@
-const Joi = require("@hapi/joi");
+const Joi = require('joi');
 
 const authSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().email().lowercase().required(),
-  //pwd must be ('^[a-zA-Z0-9]{3,30}$')
-  password: Joi.string().min(8).required(),
-  contact_number: Joi.string().required(),
-  role: Joi.string().required().valid("supplier", "retailer")
+	name: Joi.string().required(),
+	email: Joi.string()
+		.email()
+		.lowercase()
+		.required(),
+	//pwd must be ('^[a-zA-Z0-9]{3,30}$')
+	password: Joi.string().min(8).required(),
+	contact_number: Joi.string().required(),
+	role: Joi.string()
+		.required()
+		.valid('supplier', 'retailer'),
 });
 
 const loginSchema = Joi.object({
-  email: Joi.string().email().lowercase().required(),
-  //pwd must be ('^[a-zA-Z0-9]{3,30}$')
-  password: Joi.string().min(8).required(),
+	email: Joi.string()
+		.email()
+		.lowercase()
+		.required(),
+	//pwd must be ('^[a-zA-Z0-9]{3,30}$')
+	password: Joi.string().min(8).required(),
 });
 
 module.exports = {
-  authSchema,
-  loginSchema,
+	authSchema,
+	loginSchema,
 };
